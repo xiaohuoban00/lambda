d1f3ee04b21cc1fb94fc2bd6e4666f1b43b6d5ee
曾铭淇 <1475927880@qq.com>
2020-04-20 00:49:52 +0800

    添加Stream()常用Api的Demo

--- lambdaDemo03/src/main/java/com/zmq/Test3.java
+++ lambdaDemo03/src/main/java/com/zmq/Test3.java
@@ -1,5 +1,10 @@
 package com.zmq;
 
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
 /**
  * Created by IntelliJ IDEA.
  * User: zmq
@@ -46,6 +51,82 @@ package com.zmq;
 public class Test3 {
     public static void main(String[] args) {
         //1.批量数据-> Stream对象
-        
+        // 多个数据
+        Stream<String> stream = Stream.of("tom", "xiaobai", "xiaohei");
+        //数组
+        String[] stringArray = new String[]{"xueqi","baibai"};
+        Stream<String> stream1 = Arrays.stream(stringArray);
+        //列表
+        List<String> list = new ArrayList<>();
+        list.add("小白");
+        list.add("小何");
+        list.add("小黑");
+        Stream<String> stream2 = list.stream();
+        //集合
+        HashSet<String> set = new HashSet<>();
+        set.add("小白");
+        set.add("小何");
+        set.add("小黄");
+        Stream<String> stream3 = set.stream();
+        //map
+        Map<String,Integer> map = new HashMap<>();
+        map.put("xiaobai",18);
+        map.put("xiaohuang",20);
+        map.put("xiaohei",21);
+        Stream<Map.Entry<String, Integer>> stream4 = map.entrySet().stream();
+        // 2.stream对象用于基本数据类型功能封装
+        IntStream.of(new int[]{1, 2, 3}).forEach(System.out::println);
+        IntStream.range(1,5).forEach(System.out::println);
+        IntStream.rangeClosed(1,5).forEach(System.out::println);
+        // 3.stream对象转换得到指定的数据类型
+        //数组
+        //String[] strings = stream.toArray(String[]::new);
+        //字符串
+        //String collect = stream.collect(Collectors.joining());
+        //列表
+        //List<String> stringList = stream.collect(Collectors.toList());
+        //集合
+        //Set<String> stringSet = stream.collect(Collectors.toSet());
+        //map
+        //Map<String, String> stringMap = stream.collect(Collectors.toMap(x -> x, y -> "value:" + y));
+        //4. Stream中常见的api
+        List<String> accountList = new ArrayList<>();
+        accountList.add("小白");
+        accountList.add("小何");
+        accountList.add("小黑");
+        //map() 中间操作，map()方法接收一个Functional接口
+        //accountList=accountList.stream().map(account->"name:"+account).collect(Collectors.toList());
+        // filter 添加过滤条件，过滤符合条件的用户
+//        accountList = accountList.stream().filter(x->x.length()>0).collect(Collectors.toList());
+        //foreach增强型循环
+//        accountList.forEach(account-> System.out.println("ForEach->"+account));
+        // peek()中间操作，迭代数据完成数据的依次处理
+//        accountList.stream().peek(x-> System.out.println("peek1"+x))
+//        .peek(x-> System.out.println("peek2"+x))
+//        .forEach(System.out::println);
+//        accountList.forEach(System.out::println);
+
+        //Stream 中对于数字运算的支持
+        List<Integer> integerList = new ArrayList<>();
+        integerList.add(20);
+        integerList.add(21);
+        integerList.add(23);
+        integerList.add(27);
+        //skip() 中间操作，有状态，跳过部分数据
+//        integerList.stream().skip(2).forEach(System.out::println);
+        //limit() 中间操作，有状态，限制输出数据量
+//        integerList.stream().skip(2).limit(1).forEach(System.out::println);
+        // distinct() 中间操作，有状态，剔除重复数据
+//        integerList.stream().distinct().forEach(System.out::println);
+        // sorted() 中间操作，有状态，排序
+        // max() 获取最大值
+        Optional<Integer> optional = integerList.stream().max((x, y) -> x - y);
+        System.out.println(optional.get());
+        // min() 获取最小值
+        // reduce() 合并处理数据
+        Optional<Integer> optional1 = integerList.stream().reduce((sum, x) -> sum += x);
+        System.out.println(optional1.get());
     }
+
+
 }
